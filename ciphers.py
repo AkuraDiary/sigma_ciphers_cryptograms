2b3c57ee5762fa15d18b0a361fb2b14708976bae
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-11-07 15:48:33 +0700

    first commit

---
+++ ciphers.py
@@ -0,0 +1,9 @@
+class ciphers():
+    def __init__(self):
+        pass
+
+    def coder(data):
+        return data
+    
+    def decoder(data):
+        return data
---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+
+def fileIsExist(filename):
+    return os.path.exists(filename)
+
+if __name__ == '__main__':
+    print(fileIsExist("file.txt"))
