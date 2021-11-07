1bdd81007f5596b33312a2422a78398ff5b7f546
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-11-07 21:44:25 +0700

    added atbash and caesar implementation

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from caesar import caesar
 from ABZA import ABZA
 from atbash import atbash
 
+#algorithm
+Caesar = caesar()
+Atbash = atbash()
+Abza = ABZA()
+A1Z26 = A1Z26()
+
+
 def fileIsExist(filename):
     return os.path.exists(filename)
 
@@ -13,16 +20,29 @@ def readFileContent(filename):
         return f.read()
 
 def makeCopyOfFile(oldFileName, newContent, key, status = "encrypted"):
-    newFileName = status + "-"+ oldFileName + str(key)
+    newFileName = status + "-"+ oldFileName
     with open(newFileName, 'w') as f:
         f.write(key + '\n')
         f.write(newContent)
+    
+def readEncryptedFile(filename, key):
+    with open(filename, 'r') as f:
+        f.readline()
+        content =  f.read()
+        if key.upper() == "A1Z26":
+            return A1Z26.decode(content)
+        elif key.lower() == "caesar":
+            return Caesar.decode(content)
+        elif key.upper() == "ABZA":
+            return Abza.decode(content)
+        elif key.capitalize() == "Atbash":
+            return Atbash.decode(content)
+        else:
+            return "invalid key"
+    
 
-#algorithm
-Caesar = caesar()
-Atbash = atbash()
-Abza = ABZA()
-A1Z26 = A1Z26()
+        
+
 
 key = {
     "Caesar": Caesar,
@@ -35,14 +55,20 @@ if __name__ == '__main__':
     data = readFileContent('dummy-file.txt')
 
     print("dummy-file.txt")
-    print("algorithm : Caesar")
+    print("algorithm : Atbash")
     print("data : ")
     print(data)
     print()
-    print("decoded data : ")
-    decoded_caesar_data = Caesar.decode(readFileContent('dummy-file.txt'))
-    print(decoded_caesar_data)
-    makeCopyOfFile('dummy-file.txt', decoded_caesar_data, 'Caesar')
+    print("encoded data : ")
+    encoded_atbash_data = Atbash.encode(readFileContent('dummy-file.txt'))
+    print(encoded_atbash_data)
+    makeCopyOfFile('dummy-file.txt', encoded_atbash_data, 'Caesar')
+
+    print()
     print("encrypted file created")
+    print()
+
+    print("reading encrypted File : ")
+    print(readEncryptedFile('encrypted-dummy-file.txt', 'atbash'))
     
 
