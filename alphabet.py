2589ad24198a0092abe250c77460f1f7c4b8f0ed
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-11-08 09:44:58 +0700

    added symbol support

--- alphabet.py
+++ alphabet.py
@@ -2,6 +2,7 @@ class alphabet():
     uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     lowercase = "abcdefghijklmnopqrstuvwxyz"
     numbers = "0123456789"
+    symbols = "!@#$%^&*()_+-=][}{|;':\",./<>?"
 
     def Reverse(self, string):
         return string[::-1]
@@ -18,8 +19,16 @@ class alphabet():
     def getUppercaseByIndex(self, index):
         return self.uppercase[index]
 
+    def getSymbolByIndex(self, index):
+        return self.symbols[index]
+    
+    def getIndexOfSymbol(self, symbol):
+        return self.symbols.index(symbol)
+
 if __name__ == '__main__':
     a = alphabet()
+    print(a.getSymbolByIndex(16))
+    print(a.getIndexOfSymbol("}"))
 
     
     
