8ed85005aa719cca65e5f826bbb2d7d55583c25f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-11-08 09:45:21 +0700

    added symbol support for atbash algorithm

--- atbash.py
+++ atbash.py
@@ -14,8 +14,14 @@ class atbash(ciphers):
                 elif char.islower():
                     index = self._alphabet.getIndexOfLowercase(char)
                     result += self._alphabet.getLowercaseByIndex(25 - index)
+                elif char.isspace():
+                    result += " "
                 else:
                     result += char
+                    
+            elif char in self._alphabet.symbols:
+                index = self._alphabet.getIndexOfSymbol(char)
+                result += self._alphabet.getSymbolByIndex(29 - index)
             else:
                 result += char
         return result
@@ -31,8 +37,13 @@ class atbash(ciphers):
                 elif char.islower():
                     index = self._alphabet.getIndexOfLowercase(char)
                     result += self._alphabet.getLowercaseByIndex(25 - index)
+                elif char.isspace():
+                    result += " "
                 else:
                     result += char
+            elif char in self._alphabet.symbols:
+                index = self._alphabet.getIndexOfSymbol(char)
+                result += self._alphabet.getSymbolByIndex(29 - index)
             else:
                 result += char
         return result
