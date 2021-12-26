a2d90a6e3a994d57574a7ac542fa7a076713278f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-26 17:31:01 +0700

    okay it's worked

--- Sigma.py
+++ Sigma.py
@@ -24,11 +24,58 @@ class Sigma(ciphers):
     ]
 
     keys = "ABCD"
+       
+    def start_encode(self, text, _token):
+        print("ENCODING")
+        token = _token
+        print("Token : ",token)
+        # encode the text with each char of token
+        #temp_result = ""
+        result = text
+        print("starting loop \n")
+        for i in range(len(token)):
+            print(i , "loop")
+            print("text to encode : " , result)
 
-    def start_encode(self, text, token):
-        token = token.ignoercase()
+            # get the encoder class key by token
+            algo = self.get_algo_type_from_token(token, i)
+            # encode the text with the encoder class key
+            result = algo.encode(result)
+            
+            #LOGS
+            print("algo : " , algo)
+            print("result : " , result , "\n")
+        print("ending loop")
+        print("ENCODING \n")
+        return result
+    
+    def start_decode(self, text, _token):
+        print("DECODING")
+        print("Token : ", _token)
+        reversed_token = self.walik(_token)
+        print("Reversed Token : ", reversed_token)
+        # decode the text with each part of token
+        #temp_result = ""
+        result = text
+        print("starting loop \n")
+        for i in range(len(reversed_token)):
+            print(i , "loop")
+            print("text to decode : " , result)
+
+            # get the encoder class key by token
+            algo = self.get_algo_type_from_token(reversed_token, i)
+            # decode the text with the encoder class key
+            result = algo.decode(result)
+            
+            #LOGS
+            print("algo : " , algo)
+            print("result : " , result , "\n")
+        print("ending loop")
+        print("DECODING \n")
+        return result
 
     def generate_token(self, _token_length=8):
+        # generate a random token from keys with a customable length
         token = ""
         _keys = self.keys
         for i in range(_token_length):
@@ -42,10 +89,20 @@ class Sigma(ciphers):
     def get_algo_type_from_token(self, _token, _index):
         # read the char in token and return the encoder class key by index of char index in keys
         try:
-            return self.encoder_class_key[self.keys.index(_token[_index])]
+            if _token[_index] not in _token:
+                raise Exception("The token is not valid")
+            else:
+                return self.encoder_class_key[self.keys.index(_token[_index])]
         except Exception as e:
             print("Error: {}".format(e))
             return None
+        
+    def walik(self, unreversed_token):
+        # reverse the token
+        reversed_token = ""
+        for i in reversed(unreversed_token):
+            reversed_token += i
+        return reversed_token
         
             
             
@@ -53,10 +110,16 @@ class Sigma(ciphers):
 
 if __name__ == "__main__":
     #playground testing
-
     sigma = Sigma()
-    dummy_token = sigma.generate_token()
-    algo = sigma.get_algo_type_from_token(dummy_token, 0)
+    dummy_token = sigma.generate_token(_token_length=4)
+    #algo = sigma.get_algo_type_from_token(dummy_token, 0)
     print("Token:", dummy_token)
-    print("Algo from Token index 0: ", algo)
-    print(algo.encode("zabcd"))
+    #print("Algo from Token index 0: ", algo)
+    text = "Never Gonna Give You Up"
+    print("Text:", text)
+    print()
+    encoded_text = sigma.start_encode(text, dummy_token)
+    print("Encoded Text:", encoded_text , "\n")
+    decoded_text = sigma.start_decode(encoded_text, dummy_token)
+    print("Decoded Text:", decoded_text)
+    
