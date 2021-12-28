e5a357629642ec8b301be08e10d31f95875f6dc4
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-28 21:36:06 +0700

    added salt and pepper

--- Sigma.py
+++ Sigma.py
@@ -23,12 +23,16 @@ class Sigma(ciphers):
     AN
     ]
 
+    #keys
     uppercase_keys = "ABCD"
     lowercase_keys = "abcd"
     symbol_keys = "!#$%"
     space_keys = "\u025A" + "\u025B" + "\u025C" + "\u025D" + "\u025E" + "\u025F" + "\u024F"
     enter_keys = "~"+ "\u027F" + "\u028A"+ "\u018D"
 
+    #spices
+    salt = "\u00A3" + "\u00A7" + "\u00B5" + "\u00BF"
+    pepper = "\u0234" + "\u0235" + "\u0236" + "\u0237"
     
     ##CRUCIAL FUNCTION##
     def start_encode(self, text, _token):
@@ -47,19 +51,29 @@ class Sigma(ciphers):
                 result = result.replace(result[i], random.choice(self.space_keys), 1)
             elif text[i] == "\n":
                 result = result.replace(result[i], random.choice(self.enter_keys), 1)
+        
+        #adding the spices
+        for i in range(len(result)):
+            if self.should_i():
+                salt = random.choice(self.salt)
+                pepper = random.choice(self.pepper)
+                index = result.find(result[i])
+                result = result[:index] + random.choice([salt, pepper]) + result[index:]
 
-        return result
+        return str(result)
     
     def start_decode(self, text, _token):
         reversed_token = self.walik(_token)
         result = text
 
-        #checking for spaces and enters (carriage return) symbols first and encode it
+        #checking for spaces and enters (carriage return) symbols first and encode it and cleaning the spices
         for i in range(len(text)):
             if text[i] in self.space_keys:
                 result = result.replace(text[i], " ")
             elif text[i] in self.enter_keys:
                 result = result.replace(text[i], "\n")
+            elif (text[i] in self.salt) or (text[i] in self.pepper):
+                result = result.replace(text[i], "")
 
         for i in range(len(reversed_token)):
             # get the encoder class key by token
@@ -67,7 +81,7 @@ class Sigma(ciphers):
             # decode the text with the encoder class key
             result = algo.decode(result)
 
-        return result
+        return str(result)
 
     def generate_token(self, _token_length=8):
         # generate a random token from keys with a customable length
@@ -83,7 +97,7 @@ class Sigma(ciphers):
             state = the_keys
             # make sure no direct duplicate in token
             if i>0:
-                if self.should_i_remove_duplicate(): 
+                if self.should_i(): 
                     #yes i should remove duplicate
                     if token[i] == token[i-1]:
                         temp_keys = state.replace(token[i], "")#removinng duplicated char from keys
@@ -119,7 +133,7 @@ class Sigma(ciphers):
     ##CRUCIAL FUNCTION##
 
     ##UTIL FUNCTION##
-    def should_i_remove_duplicate(self):
+    def should_i(self):
         return (random.randint(1, 100))%2 == 0 # jika nilai genap return true
     
     def walik(self, unreversed_token):
