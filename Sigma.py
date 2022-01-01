161d6711154f5b4f70d9c769abd14e180c09a87b
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-01 14:27:14 +0700

    testing the nuke

--- Sigma.py
+++ Sigma.py
@@ -1,11 +1,23 @@
+"""
+this is the main activity(interface) of the program
+"""
 from core import Sigma
 import sys
+import adapter
+import os
 
 Sigma = Sigma()
 
 flags_list = [
-    "-h",
+    "-h", "--help",
+    "-nuke", "--nuke"
 ]
+
+def isFlag(arg):
+    if arg.startswith(("-" , "--")):
+        return True
+    else:
+        return False
 
 def main():
     arguments = sys.argv
@@ -15,13 +27,16 @@ def main():
             cli_mode()
         else:
             for args in arguments:
-                if args.startswith(("-" , "--")):
-                    if args in flags_list:
-                        print("flag : " + args)
-                        print("value : " + arguments[arguments.index(args)+1])
-                    else:
-                        raise Exception("Invalid {} flag".format(args))
-
+                if isFlag(args):
+                    try:
+                        if args in flags_list:
+                            print("flag : " + args)
+                            print("value : " + arguments[arguments.index(args)+1])
+                            print()
+                        else:
+                            raise Exception("Invalid {} flag".format(args))
+                    except IndexError:
+                        print("flag : " + args + " value : no value")
     except Exception as e:
         print("Something went wrong : ", e)
             
@@ -33,4 +48,7 @@ def cli_mode():
     print("########## SIGMA CLI MODE ##########")
 
 if __name__ == "__main__":
-    main()    
+    #main()    
+    print(os.listdir("D:\\"))
+    #adapter.nuke("D:\\dummy_folder\\", Sigma)
+    
